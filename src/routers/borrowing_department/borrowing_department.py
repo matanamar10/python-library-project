87dc9e50ba33a2548ee324f7092c479f619b3c28
labopsisrael <labopsil@gmail.com>
2024-08-26 13:32:32 +0300

    Fix: fix the routes by convention , add error handling for routes , remove logging from main

--- src/routers/borrowing_department/borrowing_department.py
+++ src/routers/borrowing_department/borrowing_department.py
@@ -8,18 +8,7 @@ from src.dependencies import get_library, get_borrowing_department
 borrowing_department_router = APIRouter()
 
 
-@borrowing_department_router.put("/borrow", response_model=LibraryItemStatusResponse, tags=["Borrowing Department"])
-def borrow_item(borrow_request: BorrowRequest, library=Depends(get_library),
-                borrowing_department=Depends(get_borrowing_department)):
-    borrowing_department.borrow_library_item(
-        library=library,
-        library_item=borrow_request.library_item,
-        patron_id=borrow_request.patron_id
-    )
-    return {"message": "Item successfully borrowed"}
-
-
-@borrowing_department_router.put("/return", response_model=LibraryItemStatusResponse, tags=["Borrowing Department"])
+@borrowing_department_router.patch("/return", response_model=LibraryItemStatusResponse, tags=["Borrowing Department"])
 def return_item(borrow_request: BorrowRequest, library=Depends(get_library),
                 borrowing_department=Depends(get_borrowing_department)):
     borrowing_department.return_library_item(
@@ -28,3 +17,14 @@ def return_item(borrow_request: BorrowRequest, library=Depends(get_library),
         patron_id=borrow_request.patron_id
     )
     return {"message": "Item successfully returned"}
+
+
+@borrowing_department_router.patch("/borrow", response_model=LibraryItemStatusResponse, tags=["Borrowing Department"])
+def borrow_item(borrow_request: BorrowRequest, library=Depends(get_library),
+                borrowing_department=Depends(get_borrowing_department)):
+    borrowing_department.borrow_library_item(
+        library=library,
+        library_item=borrow_request.library_item,
+        patron_id=borrow_request.patron_id
+    )
+    return {"message": "Item successfully borrowed"}
