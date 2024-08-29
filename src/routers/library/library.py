54e125018fbd8d1c58a1a38d852f191b67bb5d45
labopsisrael <labopsil@gmail.com>
2024-08-29 13:21:56 +0300

    Fix: change the example check according to all the code changes.

--- src/routers/library/library.py
+++ src/routers/library/library.py
@@ -11,6 +11,19 @@ library_router = APIRouter()
 
 @library_router.post(path="/items", response_model=NewItemsResponse, tags=["Library Items"])
 def add_library_item(add_new_item_request: AddItemRequest, library=Depends(get_library)):
+    """
+    Add new items to the library.
+
+    This endpoint allows you to add new items (e.g., books, disks) to the library's collection.
+    The items to be added are provided in the request body.
+
+    Args:
+        add_new_item_request (AddItemRequest): A request object containing the details of the items to be added.
+        library: The library instance, injected via dependency.
+
+    Returns:
+        dict: A message confirming the successful addition of the new items.
+    """
     library.add_new_library_items_to_the_library(new_library_items=add_new_item_request.library_items)
     return {
         "message": "The new items successfully added"
@@ -19,15 +32,39 @@ def add_library_item(add_new_item_request: AddItemRequest, library=Depends(get_l
 
 @library_router.post(path="/patrons", response_model=NewPatronsResponse, tags=["Library Patrons"])
 def add_patrons(add_new_patrons_request: AddPatronsRequest, library=Depends(get_library)):
+    """
+    Add new patrons to the library.
+
+    This endpoint allows you to add new patrons to the library's system.
+    The patrons to be added are provided in the request body.
+
+    Args:
+        add_new_patrons_request (AddPatronsRequest): A request object containing the details of the patrons to be added.
+        library: The library instance, injected via dependency.
+
+    Returns:
+        dict: A message confirming the successful addition of the new patrons.
+    """
     library.add_new_patron_to_the_library(patrons_to_add=add_new_patrons_request.patrons_to_add)
     return {
         "message": "The new patrons successfully added"
     }
 
 
-@library_router.delete(path="/patrons/{id}", response_model=LibraryPatronStatusResponse,
-                       tags=["Library Patrons"])
+@library_router.delete(path="/patrons/{id}", response_model=LibraryPatronStatusResponse, tags=["Library Patrons"])
 def delete_patrons(patron_id: str, library=Depends(get_library)):
+    """
+    Delete a patron from the library.
+
+    This endpoint allows you to remove a patron from the library's system using their ID.
+
+    Args:
+        patron_id (str): The ID of the patron to be removed.
+        library: The library instance, injected via dependency.
+
+    Returns:
+        dict: A message confirming the successful removal of the patron, along with the patron's ID.
+    """
     library.remove_patrons_from_the_library(patron_id=patron_id)
     return {
         "message": "Patron successfully removed",
@@ -37,6 +74,18 @@ def delete_patrons(patron_id: str, library=Depends(get_library)):
 
 @library_router.delete(path="/items/{item_isbn}", response_model=LibraryItemStatusResponse, tags=["Library Items"])
 def delete_items(item_isbn: str, library=Depends(get_library)):
+    """
+    Delete an item from the library.
+
+    This endpoint allows you to remove an item from the library's collection using its ISBN.
+
+    Args:
+        item_isbn (str): The ISBN of the item to be removed.
+        library: The library instance, injected via dependency.
+
+    Returns:
+        dict: A message confirming the successful removal of the item, along with the item's ISBN.
+    """
     library.remove_items_from_the_library(item_isbn_to_remove=item_isbn)
     return {
         "message": "Item successfully removed from the library",
@@ -46,11 +95,35 @@ def delete_items(item_isbn: str, library=Depends(get_library)):
 
 @library_router.get(path="/items/{isbn}", response_model=LibraryItemResponse, tags=["Library Items"])
 def get_library_item(item_isbn: str, library=Depends(get_library)):
+    """
+    Get details of a library item.
+
+    This endpoint retrieves the details of a specific library item using its ISBN.
+
+    Args:
+        item_isbn (str): The ISBN of the item to retrieve.
+        library: The library instance, injected via dependency.
+
+    Returns:
+        dict: A dictionary containing the details of the requested library item.
+    """
     item = library.library_items[item_isbn]
     return {"item": item}
 
 
 @library_router.get(path="/patrons/{id}", response_model=PatronResponse, tags=["Library Patrons"])
 def get_library_patron(patron_id: str, library=Depends(get_library)):
+    """
+    Get details of a library patron.
+
+    This endpoint retrieves the details of a specific patron using their ID.
+
+    Args:
+        patron_id (str): The ID of the patron to retrieve.
+        library: The library instance, injected via dependency.
+
+    Returns:
+        dict: A dictionary containing the details of the requested patron.
+    """
     patron = library.patrons[patron_id]
     return {"patron": patron}
