bce098b57df3f567ef438722c849f970ebfbb706
matanamar10 <matanamar76@gmail.com>
2024-09-18 13:13:43 +0300

    Fix: add Library application configuration to .env .

--- src/main.py
+++ src/main.py
@@ -1,14 +1,16 @@
 from src.app import create_app
 from src.mongodb.mongo_setup import connect_to_mongodb, disconnect_from_mongodb
 from utils.custom_library_errors import *
+from env_utils import MainSettings
 import uvicorn
 
 
 def main():
     try:
+        settings=MainSettings()
         connect_to_mongodb()
-        app = create_app(library_name="AmarLibrary")
-        uvicorn.run(app, host="127.0.0.1", port=8000)
+        app = create_app(library_name=settings.library_name)
+        uvicorn.run(app, host=settings.host, port=settings.port)
     except LibraryError:
         raise
     except Exception:
